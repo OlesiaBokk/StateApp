49f97cfc57932e44d4562bcb0c5ef131b9ca0a34
OlesiaBokk <orbokk@gmail.com>
2024-04-02 18:58:17 +0300

    Added generator of name/surname, randomChar and randomNumber for createName generators

--- src/main/java/cy/olesiabokk/stateapp/entity/Citizen.java
+++ src/main/java/cy/olesiabokk/stateapp/entity/Citizen.java
@@ -1,13 +1,47 @@
 package cy.olesiabokk.stateapp.entity;
 
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Citizen {
-    private int id;
+    private long id;
+    private static AtomicLong counter = new AtomicLong(0);
+
     private String name;
     private String surname;
     private int age;
     private State state;
 
-    public int getId() {
+    public Citizen() {
+        this.id = createID();
+        this.name = createName();
+        this.surname = createName();
+    }
+
+    private static long createID() {
+        return counter.addAndGet(1);
+    }
+
+    private String createName() {
+        char[] strName = new char[getRandomNumber()];
+        for (int i = 0; i < strName.length; i++) {
+            strName[i] = getRandomChar();
+        }
+        return name = String.valueOf(strName);
+    }
+
+    private char getRandomChar() {
+        Random r = new Random();
+        return (char) (r.nextInt(26) + 'a');
+    }
+
+    private int getRandomNumber() {
+        Random random = new Random();
+        return (4 + random.nextInt(11 - 4));
+    }
+
+
+    public long getId() {
         return id;
     }
 
@@ -26,6 +60,4 @@ public class Citizen {
     public State getState() {
         return state;
     }
-
-
 }
