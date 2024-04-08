91fd4dfc5e3bbdfaa80398c5ec54a565166ee5da
OlesiaBokk <orbokk@gmail.com>
2024-04-08 21:21:54 +0300

    Фdd filling cities with citizens

--- src/main/java/cy/olesiabokk/stateapp/entity/City.java
+++ src/main/java/cy/olesiabokk/stateapp/entity/City.java
@@ -3,16 +3,29 @@ package cy.olesiabokk.stateapp.entity;
 import cy.olesiabokk.stateapp.entity.interfaces.General;
 import cy.olesiabokk.stateapp.util.Generator;
 
+import java.util.ArrayList;
+
 public class City implements General {
     private String name;
     private Generator generator = new Generator();
 
+    private final ArrayList<Citizen> citizens;
+
     public City() {
         this.name = generator.generateName();
+        this.citizens = new ArrayList<>();
     }
 
     @Override
     public String getName() {
         return name;
+    }
+
+    public ArrayList<Citizen> getCitizens() {
+        return new ArrayList<>(citizens);
+    }
+
+    public void addCitizens(ArrayList<Citizen> citizens) {
+        this.citizens.addAll(citizens);
     }
 }
--- src/main/java/cy/olesiabokk/stateapp/entity/State.java
+++ src/main/java/cy/olesiabokk/stateapp/entity/State.java
@@ -7,17 +7,21 @@ import cy.olesiabokk.stateapp.util.Generator;
 import java.util.ArrayList;
 
 public class State implements General {
-    private static final State instance = new State(new EntityGenerator().createRegionCollection());
+    private static final State instance = new State();
     private final String name;
     private final String capitalName;
     private Capital capital;
     private final Generator generator = new Generator();
     private final ArrayList<Region> regions;
 
-    private State(ArrayList<Region> regions) {
+    private State() {
         this.name = generator.generateName();
-        this.regions = new ArrayList<>(regions);
+        this.regions = new EntityGenerator().createRegionCollection();
         this.capitalName = getCapital();
+        regions.stream()
+                .flatMap(region -> region.getDistricts().stream())
+                .flatMap(district -> district.getCities().stream())
+                .forEach(city -> city.addCitizens(new EntityGenerator().createCitizenCollection()));
     }
 
     public static State getInstance() {
