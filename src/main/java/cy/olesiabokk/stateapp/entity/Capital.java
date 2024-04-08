c8e911aa622fdcc08f99019be550ca5fe0c38b01
OlesiaBokk <orbokk@gmail.com>
2024-04-08 18:46:39 +0300

    Add Capital class

---
+++ src/main/java/cy/olesiabokk/stateapp/entity/Capital.java
@@ -0,0 +1,14 @@
+package cy.olesiabokk.stateapp.entity;
+
+public class Capital extends City{
+    private static Capital instance;
+   private Capital(){
+       super();
+   }
+    public static Capital getInstance() {
+        if (instance == null) {
+            instance = new Capital();
+        }
+        return instance;
+    }
+}
