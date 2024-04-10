90d7461202fd95852f12fd11d8831aeaaa13325f
OlesiaBokk <orbokk@gmail.com>
2024-04-10 19:36:43 +0300

    Set Generators as variables in State() constructor, add Capital setter. Updated fields modificators.

--- src/main/java/cy/olesiabokk/stateapp/model/entity/State.java
+++ src/main/java/cy/olesiabokk/stateapp/model/entity/State.java
@@ -8,20 +8,22 @@ import java.util.ArrayList;
 
 public class State implements General {
     private static final State instance = new State();
-    private static int area;
+    private final int area;
     private final String name;
-    private Capital capital;
-    private final Generator generator = new Generator();
+    private final Capital capital;
     private final ArrayList<Region> regions;
 
     private State() {
+        Generator generator = new Generator();
+        EntityGenerator entityGenerator = new EntityGenerator();
+        this.regions = entityGenerator.createRegionCollection();
         this.name = generator.generateName();
-        this.regions = new EntityGenerator().createRegionCollection();
         this.area = generator.getRandomNumber(1000, 10000000);
         regions.stream()
                 .flatMap(region -> region.getDistricts().stream())
                 .flatMap(district -> district.getCities().stream())
                 .forEach(city -> city.addCitizens(new EntityGenerator().createCitizenCollection()));
+        this.capital = Capital.getInstance();
     }
 
     public static State getInstance() {
