91196621d6d273018795694980152711ca3f917d
OlesiaBokk <orbokk@gmail.com>
2024-04-11 21:57:39 +0300

    Changed List to ArrayList. rewrite methods getResidentAverageAge(), getResByLettersNumber(), getResNamesByFirstChar(): changed .stream  to fori cycle. Add getCitizenList()

--- src/main/java/cy/olesiabokk/stateapp/model/service/StateServiceImpl.java
+++ src/main/java/cy/olesiabokk/stateapp/model/service/StateServiceImpl.java
@@ -1,9 +1,8 @@
 package cy.olesiabokk.stateapp.model.service;
 
-import cy.olesiabokk.stateapp.model.entity.Citizen;
-import cy.olesiabokk.stateapp.model.entity.State;
+import cy.olesiabokk.stateapp.model.entity.*;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class StateServiceImpl implements StateService {
     private final State state;
@@ -29,42 +28,67 @@ public class StateServiceImpl implements StateService {
 
     @Override
     public long regionCount() {
-        return state.getRegions().stream().count();
+        return state.getRegions().size();
     }
 
     @Override
     public int getResidentAverageAge() {
-        List<Integer> allCitizenAge = state.getRegions().stream()
-                .flatMap(region -> region.getDistricts().stream())
-                .flatMap(district -> district.getCities().stream())
-                .flatMap(city -> city.getCitizens().stream())
-                .mapToInt(Citizen::getAge)
-                .boxed().toList();
-
-        return (allCitizenAge.stream()
-                .mapToInt(el -> el).sum() / allCitizenAge.size());
+        ArrayList<Citizen> citizens = getCitizenList();
+        int sum = 0;
+        for (int i = 0; i < citizens.size(); i++) {
+            sum = sum + citizens.get(i).getAge();
+        }
+        return sum / citizens.size();
     }
 
     @Override
-    public List<String> getResByLettersNumber(int number) {
-        return state.getRegions().stream()
-                .flatMap(region -> region.getDistricts().stream())
-                .flatMap(district -> district.getCities().stream())
-                .flatMap(city -> city.getCitizens().stream())
-                .map(Citizen::getName)
-                .filter(name -> name.toCharArray().length == number)
-                .toList();
-
+    public ArrayList<Citizen> getResByLettersNumber(int number) {
+        ArrayList<Citizen> citizens = getCitizenList();
+        ArrayList<Citizen> citizensStartLetter = new ArrayList<>();
+        for (int i = 0; i < citizens.size(); i++) {
+            if (citizens.get(i).getName().toCharArray().length == number) {
+                citizensStartLetter.add(citizens.get(i));
+            }
+        }
+        return citizensStartLetter;
     }
 
     @Override
-    public List<String> getResNamesByFirstChar(char character) {
-        return state.getRegions().stream()
-                .flatMap(region -> region.getDistricts().stream())
-                .flatMap(district -> district.getCities().stream())
-                .flatMap(city -> city.getCitizens().stream())
-                .map(Citizen::getName)
-                .filter(name -> name.charAt(0) == character)
-                .toList();
+    public ArrayList<Citizen> getResNamesByFirstChar(char character) {
+        ArrayList<Citizen> citizens = getCitizenList();
+        ArrayList<Citizen> citizenByFirstChar = getCitizenList();
+        for (int i = 0; i < citizens.size(); i++) {
+            if (citizens.get(i).getName().charAt(0) == character) {
+                citizenByFirstChar.add(citizens.get(i));
+            }
+        }
+        return citizenByFirstChar;
+    }
+
+    public ArrayList<District> getDistrictList() {
+        ArrayList<Region> regions = state.getRegions();
+        ArrayList<District> districts = new ArrayList<>();
+        for (int i = 0; i < regions.size(); i++) {
+            districts.addAll(regions.get(i).getDistricts());
+        }
+        return districts;
+    }
+
+    public ArrayList<City> getCityList() {
+        ArrayList<District> districts = getDistrictList();
+        ArrayList<City> cities = new ArrayList<>();
+        for (int i = 0; i < districts.size(); i++) {
+            cities.addAll(districts.get(i).getCities());
+        }
+        return cities;
+    }
+
+    public ArrayList<Citizen> getCitizenList() {
+        ArrayList<City> cities = getCityList();
+        ArrayList<Citizen> citizens = new ArrayList<>();
+        for (int i = 0; i < cities.size(); i++) {
+            citizens.addAll(cities.get(i).getCitizens());
+        }
+        return citizens;
     }
 }
